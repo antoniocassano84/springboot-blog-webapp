010c7ccd2366ee09f1aefa4c33e6c49d5c026318
Antonio Cassano <antonio.cassano@nhs.net>
2023-01-16 14:34:06 +0000

    added posts.html view

--- src/main/java/net/javaguides/springboot/service/impl/PostServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/impl/PostServiceImpl.java
@@ -3,7 +3,7 @@ package net.javaguides.springboot.service.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 import net.javaguides.springboot.dto.PostDto;
-import net.javaguides.springboot.mapper.PostMapperStruct;
+import net.javaguides.springboot.mapper.PostMapper;
 import net.javaguides.springboot.repository.PostRepository;
 import net.javaguides.springboot.service.PostService;
 import org.springframework.stereotype.Service;
@@ -12,17 +12,13 @@ import org.springframework.stereotype.Service;
 public class PostServiceImpl implements PostService {
 
   private PostRepository postRepository;
-  private PostMapperStruct postMapperStruct;
-
-  public PostServiceImpl(PostRepository postRepository,
-      PostMapperStruct postMapperStruct) {
+  public PostServiceImpl(PostRepository postRepository) {
     this.postRepository = postRepository;
-    this.postMapperStruct = postMapperStruct;
   }
 
   @Override
   public List<PostDto> findAllPosts() {
     return postRepository.findAll().stream()
-        .map(postMapperStruct::mapToPostDto).collect(Collectors.toList());
+        .map(PostMapper::mapToPostDto).collect(Collectors.toList());
   }
 }
