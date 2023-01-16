d151692f1a8c1277f04bbe6e5516ce6f6558dad1
Antonio Cassano <antonio.cassano@nhs.net>
2023-01-16 16:36:07 +0000

    fixed mapper struct

--- src/main/java/net/javaguides/springboot/service/impl/PostServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/impl/PostServiceImpl.java
@@ -1,9 +1,8 @@
 package net.javaguides.springboot.service.impl;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import net.javaguides.springboot.dto.PostDto;
-import net.javaguides.springboot.mapper.PostMapper;
+import net.javaguides.springboot.mapper.PostMapperStruct;
 import net.javaguides.springboot.repository.PostRepository;
 import net.javaguides.springboot.service.PostService;
 import org.springframework.stereotype.Service;
@@ -12,13 +11,16 @@ import org.springframework.stereotype.Service;
 public class PostServiceImpl implements PostService {
 
   private PostRepository postRepository;
-  public PostServiceImpl(PostRepository postRepository) {
+  private PostMapperStruct postMapperStruct;
+  public PostServiceImpl(PostRepository postRepository,
+                         PostMapperStruct postMapperStruct) {
     this.postRepository = postRepository;
+    this.postMapperStruct = postMapperStruct;
   }
 
   @Override
   public List<PostDto> findAllPosts() {
     return postRepository.findAll().stream()
-        .map(PostMapper::mapToPostDto).collect(Collectors.toList());
+        .map(postMapperStruct::mapToPostDto).toList();
   }
 }
