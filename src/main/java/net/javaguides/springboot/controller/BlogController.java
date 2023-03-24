050c1ca22c39a53344e97f40495975846423a4d3
Antonio Cassano <antonio.cassano@nhs.net>
2023-03-24 15:58:07 +0000

    Implemented blog_post.html

--- src/main/java/net/javaguides/springboot/controller/BlogController.java
+++ src/main/java/net/javaguides/springboot/controller/BlogController.java
@@ -5,6 +5,7 @@ import net.javaguides.springboot.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @AllArgsConstructor
@@ -12,6 +13,7 @@ public class BlogController {
 
     public static final String BLOG_VIEW_POSTS = "blog/view_posts";
     public static final String POST_URL = "/post/{postUrl}";
+    public static final String BLOG_POST = "blog/blog_post";
 
     private final PostService postService;
 
@@ -19,5 +21,11 @@ public class BlogController {
     public String viewBlogPosts(Model model) {
         model.addAttribute("posts", postService.findAllPosts());
         return BLOG_VIEW_POSTS;
+    }
+
+    @GetMapping(POST_URL)
+    public String showPost(@PathVariable("postUrl") String postUrl, Model model) {
+        model.addAttribute("post", postService.findPostByUrl(postUrl));
+        return BLOG_POST;
     }
 }
