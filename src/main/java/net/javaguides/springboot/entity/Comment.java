6734ef89111d3434886adbed09b4c30166b8a1ef
antonio <antonio.cassano984@gmail.com>
2023-03-24 21:59:48 +0000

    Created Comment.java entity

---
+++ src/main/java/net/javaguides/springboot/entity/Comment.java
@@ -0,0 +1,27 @@
+package net.javaguides.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+@Entity @Table(name = "comments")
+public class Comment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Lob
+    private String content;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+    @ManyToOne @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
--- src/main/java/net/javaguides/springboot/entity/Post.java
+++ src/main/java/net/javaguides/springboot/entity/Post.java
@@ -1,13 +1,11 @@
 package net.javaguides.springboot.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -32,5 +30,8 @@ public class Post {
   private LocalDateTime createdOn;
   @UpdateTimestamp
   private LocalDateTime updatedOn;
+
+  @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+  private Set<Comment> comments = new HashSet<>();
 
 }
