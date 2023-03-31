e2b684f908246351441a5249f29c023bf650a717
antonio <antonio.cassano984@gmail.com>
2023-03-31 22:10:44 +0100

    added spring config

---
+++ src/main/java/net/javaguides/springboot/config/WebSpringSecurity.java
@@ -0,0 +1,21 @@
+package net.javaguides.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSpringSecurity {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/admin/posts")
+                        .loginProcessingUrl("/login").permitAll());
+        return http.build();
+    }
+}
