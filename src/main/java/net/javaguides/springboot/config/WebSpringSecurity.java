f4781bccb1a27133614695354883061ec0b96ac9
Antonio Cassano <antonio.cassano@nhs.net>
2023-04-03 15:09:43 +0100

    database authentication

--- src/main/java/net/javaguides/springboot/config/WebSpringSecurity.java
+++ src/main/java/net/javaguides/springboot/config/WebSpringSecurity.java
@@ -1,15 +1,29 @@
 package net.javaguides.springboot.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class WebSpringSecurity {
+
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -28,5 +42,10 @@ public class WebSpringSecurity {
                 .permitAll()
             );
         return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
--- src/main/java/net/javaguides/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/impl/UserServiceImpl.java
@@ -8,21 +8,23 @@ import net.javaguides.springboot.entity.User;
 import net.javaguides.springboot.repository.RoleRepository;
 import net.javaguides.springboot.repository.UserRepository;
 import net.javaguides.springboot.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
-  UserRepository userRepository;
-  RoleRepository roleRepository;
+  private final UserRepository userRepository;
+  private final RoleRepository roleRepository;
+  private final PasswordEncoder passwordEncoder;
 
   @Override
   public void saveUser(RegistrationDto registrationDto) {
     User user = new User();
     user.setName(registrationDto.getFirstName() + " " + registrationDto.getLastName());
     user.setEmail(registrationDto.getEmail());
-    user.setPassword(registrationDto.getPassword());
+    user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
     user.setRoles(List.of(roleRepository.findByName("ROLE_GUEST")));
     userRepository.save(user);
   }
